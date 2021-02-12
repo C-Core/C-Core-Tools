26c738e4a564de1fa45e4c1a60837a990bec11f5
C-Core <clemenskern@gmail.com>
2021-02-12 23:38:59 +0100

    Batch export collections

--- __init__.py
+++ __init__.py
@@ -23,7 +23,10 @@ bl_info = {
 }
 
 import bpy
-from . import cct_ops
+from . import cct_export, cct_ops
+
+class CCORETOOLS_BatchExportProps(bpy.types.PropertyGroup):
+    path: bpy.props.StringProperty(name="Path", default="//", subtype='DIR_PATH')
 
 class CCORETOOLS_PT_MainPanel(bpy.types.Panel):
     """Add-on"""
@@ -46,13 +49,21 @@ class CCORETOOLS_PT_MainPanel(bpy.types.Panel):
         layout.operator('ccoretools.vegetation_normals')
 
         layout.separator()
+        
+        layout.label(text="Batch Export Collections")
+
+        props = context.scene.cct_export_props
+        layout.prop(props, "path", text="Path")
+        layout.operator("ccoretools.batch_export")
 
 
 classes = (
+    cct_export.CCORETOOLS_OT_BatchExport,
     cct_ops.CCORETOOLS_OT_OriginToSelected,
     cct_ops.CCORETOOLS_OT_OriginToLowestUv,
     cct_ops.CCORETOOLS_OT_VegetationVertexColors,
     cct_ops.CCORETOOLS_OT_VegetationNormals,
+    CCORETOOLS_BatchExportProps,
     CCORETOOLS_PT_MainPanel
 )
 
@@ -60,6 +71,10 @@ def register():
     for c in classes:
         bpy.utils.register_class(c)
 
+    bpy.types.Scene.cct_export_props = bpy.props.PointerProperty(type=CCORETOOLS_BatchExportProps)
+
 def unregister():
     for c in reversed(classes):
         bpy.utils.unregister_class(c)
+
+    del bpy.types.Scene.cct_export_props
---
+++ cct_export.py
@@ -0,0 +1,52 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import bpy
+import math
+import random
+from mathutils import Color, Vector, Matrix
+
+class CCORETOOLS_OT_BatchExport(bpy.types.Operator):
+    bl_idname = 'ccoretools.batch_export'
+    bl_label = 'Batch Export'
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        props = context.scene.cct_export_props
+
+        oldActiveLayer = context.view_layer.active_layer_collection
+
+        for layerCollection in context.view_layer.layer_collection.children:
+            if not layerCollection.has_objects():
+                continue
+
+            if not layerCollection.visible_get():
+                continue
+
+            context.view_layer.active_layer_collection = layerCollection
+            collection = layerCollection.collection
+
+            obj = collection.all_objects.values()[0]
+            oldLocation = obj.location.copy()
+            obj.location = (0, 0, 0)
+
+            filepath = bpy.path.abspath(props.path) + collection.name + '.fbx'
+            #print(filepath)
+            bpy.ops.export_scene.fbx(filepath=filepath, check_existing=False, use_active_collection=True)
+
+            # restore location
+            obj.location = oldLocation
+
+        context.view_layer.active_layer_collection = oldActiveLayer
+
+        return {'FINISHED'}
